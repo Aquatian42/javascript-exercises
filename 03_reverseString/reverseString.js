ab8b2eedda14f660dcc260acede6300a03aca81d
Aquatian42 <tomtomdin@gmail.com>
2025-04-04 23:58:18 +0100

    asd

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringInput) {
+    let output = "";
+    for (let i = stringInput.length -1; i >= 0;i--){
+        output += stringInput[i]
+    }
+    return output
 };
 
 // Do not edit below this line
